4813be786449271492bb571d33722e4c95a82723
kingjessie <jessie.kinganga@gmail.com>
2022-04-27 00:50:33 +0100

    update init file

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:Jdot1991@localhost/green_team"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost/green_team"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'Pa$$word'
 
